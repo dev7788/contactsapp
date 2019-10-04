2845b3946bd942515d3a8fecab8504af15749e8f
dev7788 <ivanov.artyom@europe.com>
2019-10-04 10:36:53 -0400

    added side popup

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -2,26 +2,31 @@ class Api::V1::ContactsController < ApplicationController
   def index
     render json: { :contacts => [
       {
+        :id => 1,
         :name => 'demo1',
         :email => 'demo1@gmail.com',
         :phone => '123456'
       },
       {
+        :id => 2,
         :name => 'demo2',
         :email => 'demo2@gmail.com',
         :phone => '223456'
       },
       {
+        :id => 3,
         :name => 'demo3',
         :email => 'demo3@gmail.com',
         :phone => '323456'
       },
       {
+        :id => 4,
         :name => 'demo4',
         :email => 'demo4@gmail.com',
         :phone => '423456'
       },
       {
+        :id => 5,
         :name => 'demo5',
         :email => 'demo5@gmail.com',
         :phone => '523456'
--- app/javascript/components/Contacts.js
+++ app/javascript/components/Contacts.js
@@ -3,37 +3,93 @@ import PropTypes from "prop-types"
 import { connect } from 'react-redux';
 import { Container } from 'reactstrap';
 import ReactTable from 'react-table';
-
+import { slide as Menu } from 'react-burger-menu'
 import { contactsActions } from '../actions';
 
 class Contacts extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      isOpen: false,
+      selected: null,
+      contact: null,
+    }
+
+    this.onMenuStateChange = this.onMenuStateChange.bind(this);
+  }
   componentDidMount() {
     this.props.dispatch(contactsActions.getAll());
   }
 
+  onMenuStateChange(state){
+    this.setState({isOpen: state.isOpen});
+  }
+
   render () {
-    const { loading, data } = this.props;    
-    console.log(loading, data);
+    const { loading, data } = this.props;
 
     const columns = [{
-      Header: 'Name',
-      accessor: 'name' // String-based value accessors!
-    }, {
       Header: 'Email',
-      accessor: 'email' // String-based value accessors!
+      accessor: 'email'
+    }, {
+      Header: 'Name',
+      accessor: 'name'
     }, {
       Header: 'Phone',
-      accessor: 'phone' // String-based value accessors!
+      accessor: 'phone'
     }];
 
+    const { contact } = this.state;
+
     return (
-      <Container>
-        { data && (
-        <ReactTable
-            data={data}
-            columns={columns}
-        />) }
-      </Container>
+      <div>
+        <div id="outer-container">
+          <Menu
+            pageWrapId={"page-wrap"}
+            outerContainerId={"outer-container"}
+            right
+            isOpen={this.state.isOpen}
+            width={280}
+            onStateChange={this.onMenuStateChange}
+          >
+            <div className="menu-content">
+              {contact && (
+                <span><b>{contact.name}</b></span>
+              )}
+            </div>
+          </Menu>
+          <div id="page-wrap" />
+        </div>
+        <Container>
+          {data && (
+            <ReactTable
+              data={data}
+              columns={columns}
+              getTrProps={(state, rowInfo) => {
+                if (rowInfo && rowInfo.row) {
+                  return {
+                    onClick: (e) => {
+                      console.log('rowInfo', rowInfo, e);
+                      this.setState({
+                        selected: rowInfo.index,
+                        contact: rowInfo.original,
+                        isOpen: true
+                      })
+                    },
+                    style: {
+                      background: rowInfo.index === this.state.selected ? '#00afec' : 'white',
+                      color: rowInfo.index === this.state.selected ? 'white' : 'black'
+                    }
+                  }
+                } else {
+                  return {}
+                }
+              }}
+            />)}
+        </Container>
+      </div>
+      
     );
   }
 }
